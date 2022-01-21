2bd75ee7276a6d1a5a0b18e01865f15142d5fc38
gabrieldifini <gabriel.di.fini@outlook.com>
2022-01-21 05:33:17 -0300

    Changes and fixes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const cors = require('cors')
 const mysql = require('mysql')
 const myconn = require('express-myconnection')
 const env = require('dotenv').config().parsed
-const cron = require('./cron/stats')
 const routes = require(env.NODE_ENV_ROUTES_PATH)
+
+require('./cron/stats')
 
 const app = express()
 
@@ -16,9 +16,8 @@ app.use(myconn(mysql, {
     database: env.MYSQL_DB_NAME,
 }))
 
-app.use(cors())
 app.use(routes)
 
-app.listen(env.PORT, () => {
+app.listen(env.NODE_ENV_PORT, () => {
     console.log(`Server running on http://localhost:${env.NODE_ENV_PORT}`)
 })
