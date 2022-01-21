1cff7ce3ae40587aaeffcb888b19e5c5f3951c26
gabrieldifini <gabriel.di.fini@outlook.com>
2022-01-21 08:29:11 -0300

    Corrected associations

--- server/models/player.js
+++ server/models/player.js
@@ -1,7 +1,13 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Player extends Model {}
+  class Player extends Model {
+    static associate(models) {
+      this.hasOne(models.Stat, {
+        onDelete: 'CASCADE',
+      });
+    }
+  }
   Player.init(
     {
       nickname: DataTypes.STRING,
