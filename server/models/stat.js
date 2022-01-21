d5d0f2c82d817d2a988bc0b8f091abd81a4b12bb
gabrieldifini <gabriel.di.fini@outlook.com>
2022-01-21 08:29:01 -0300

    Corrected associations

--- server/models/stat.js
+++ server/models/stat.js
@@ -3,9 +3,7 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Stat extends Model {
     static associate(models) {
-      this.belongsTo(models.Player, {
-        foreignKey: "player_id",
-      });
+      this.belongsTo(models.Player);
     }
   }
   Stat.init(
